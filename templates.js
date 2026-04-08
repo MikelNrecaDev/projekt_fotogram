36690495090985705ac70f89a46e857e56f3a97a
MikelNrecaDev <mikelnreca@hotmail.com>
2026-04-09 00:23:13 +0200

    templates klassen erstellt für buttons

--- templates.js
+++ templates.js
@@ -1,29 +1,28 @@
-function init() {
-    initimages();
-}
-
-
-function initimages() {
-    const galleryContainer = document.getElementById('gallery');
-    galleryContainer.innerHTML = "";
-
-    for (let i = 0; i < images.length; i++) {
-        const image = images[i];
-        galleryContainer.innerHTML += `<button class="picture-card"><img src="${image}" id="picturecard" onclick="OpenDetailView" alt=""></button`;
-    }
+function generateImageCardHTML(i, image) {
+   return `
+        <button class="picture-card">
+        <img src="${image}" id="picturecard" onclick="OpenDetailView('${i}'}" alt="">
+    </button
+    `;
 }
 
 function OpenDetailView() {
-        document.getElementById('picturecard').classList.add('detail-box');
-    }
-
-
-
-
-
-function closeDetailView() {
-    document.getElementById('close').onclick = function () {
-        document.getElementById('detail-box').classList.add('d-none');
-
-    }
+    console.log("aktueller Index ", currentIndex);
+    return `
+        <detail>
+            <div class="img-wrapper">
+                <img src="${image}" alt="">
+            </div>
+                  <button id="backward" class="left_button">
+                    <img src="./assets/img/PfeilLinks.png"> 
+                </button>
+                <button id="forward" class="right_button">
+                    <img src="./assets/img/PfeilRechts.png">
+                </button>
+                <button id="close" class="close_window" onclick="closeDetailView()">
+                    <img src="./assets/img/Exit.png" alt="">
+                </button>
+            
+        </detail>
+    `;
 }
