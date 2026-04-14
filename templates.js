a34427c5b82f2845645b7530dae5ee92777856ed
MikelNrecaDev <mikelnreca@hotmail.com>
2026-04-15 01:20:07 +0200

    (anpassung)

--- templates.js
+++ templates.js
@@ -1,22 +1,22 @@
 function generateImageCardHTML(i, image) {
    return `
         <button class="picture-card">
-        <img src="${image}" id="picturecard" onclick="OpenDetailView('${i}'}" alt="">
+        <img src="${image}" id="picturecard" onclick="openDetailView('${i}')" alt="">
     </button
     `;
 }
 
-function OpenDetailView() {
-    console.log("aktueller Index ", currentIndex);
+function generateDetailViewHTML(image) {
+    console.log("aktueller Index: ", currentIndex);
     return `
         <detail>
             <div class="img-wrapper">
                 <img src="${image}" alt="">
             </div>
-                  <button id="backward" class="left_button">
+                  <button onclick="previousImage" id="backward" class="left_button">
                     <img src="./assets/img/PfeilLinks.png"> 
                 </button>
-                <button id="forward" class="right_button">
+                <button onclick="nextImage" id="forward" class="right_button">
                     <img src="./assets/img/PfeilRechts.png">
                 </button>
                 <button id="close" class="close_window" onclick="closeDetailView()">
