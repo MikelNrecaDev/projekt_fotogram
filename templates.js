ee86f9b918fb23f21f452d763594266f285c1114
MikelNrecaDev <mikelnreca@hotmail.com>
2026-05-12 00:04:15 +0200

    (backward)

--- templates.js
+++ templates.js
@@ -2,21 +2,20 @@ function generateImageCardHTML(i, images) {
    return `
         <button class="picture-card">
         <img src="${images}" id="picturecard" onclick="openDetailView(${i})" alt="">
-    </button
+    </button>
     `;
 }
 
 function generateDetailViewHTML(image, index) {
-    console.log("aktueller Index: ", index);
     return `
         <detail>
             <div class="img-wrapper">
-                <img src="${image}" alt="">
+                <img id="openImage" src="${image}" alt="">
             </div>
-                  <button onclick="previousImage(${index})" id="backward" class="left_button">
+                  <button onclick="previousImage(${index})" id="openImage" class="left_button">
                     <img src="./assets/img/PfeilLinks.png"> 
                 </button>
-                <button onclick="nextImage(${index})" id="forward" class="right_button">
+                <button onclick="nextImage(${index})" id="openImage" class="right_button">
                     <img src="./assets/img/PfeilRechts.png">
                 </button>
                 <button id="close" class="close_window" onclick="closeDetailView()">
