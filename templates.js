7aa0fa59a2ce4f892d6f97cb60561faa738c60ed
MikelNrecaDev <mikelnreca@hotmail.com>
2026-07-01 01:02:34 +0200

    (nummerierung_programmiert)

--- templates.js
+++ templates.js
@@ -12,12 +12,15 @@ function generateDetailViewHTML(image, index) {
             <div class="img-wrapper">
                 <img id="openImage" src="${image}" alt="">
             </div>
+            <div class="buttons">
                   <button onclick="previousImage(${index})" id="openImage" class="left_button">
                     <img src="./assets/img/PfeilLinks.png"> 
                 </button>
+                <p class="numbers" id="nextNumber">${index + 1}/${images.length}</p>
                 <button onclick="nextImage(${index})" id="openImage" class="right_button">
                     <img src="./assets/img/PfeilRechts.png">
                 </button>
+            </div>
                 <button id="close" class="close_window" onclick="closeDetailView()">
                     <img src="./assets/img/Exit.png" alt="">
                 </button>
